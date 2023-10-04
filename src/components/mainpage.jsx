5b2372252ff140b71ad4297ff71229bc45ec08a2
Felizdujadin <ezeikefelix51@gmail.com>
2023-10-04 15:19:05 +0100

    sleeck folder updates

---
+++ src/components/mainpage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function MainPage(){
+    return(
+        <div className="main_page_">
+            Hello Zainab
+        </div>
+    )
+}
+export default MainPage;
